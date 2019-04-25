d3b20b79fd4e6869f24e6f3a64fdadabfabbb51c
Esa Dewa Dalla <esa@btoz.co.id>
2019-04-25 17:05:20 +0700

    fixing API kategori

--- app/Http/Resources/CategoryCollection.php
+++ app/Http/Resources/CategoryCollection.php
@@ -14,8 +14,6 @@ class CategoryCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return [
-           $this->collection,
-        ];
+        return $this->collection;
     }
 }
